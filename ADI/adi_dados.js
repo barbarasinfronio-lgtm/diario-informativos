be09be686527ba74dcc9a548892c31bdc8e360a1
barbarasinfronio-lgtm <barbara.sinfronio@gmail.com>
2026-09-26 06:54:14 -0300

    Create adi_dados.js

---
+++ ADI/adi_dados.js
@@ -0,0 +1,34 @@
+// adi_dados.js - Base de Dados de ADIs julgadas pelo STF
+const DADOS_ADIS = [
+  {
+    numero: 4439,
+    ano: 2017,
+    tema: "Ensino religioso confessional nas escolas públicas",
+    relator: "Min. Roberto Barroso",
+    dataJulgamento: "27/09/2017",
+    resultado: "Improcedente", // Procedente, Improcedente, Parcialmente Procedente
+    dispositivo: "Faculdade de matrícula; natureza confessional do ensino religioso nas escolas públicas mantida constitucional.",
+    linkSTF: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=3921764"
+  },
+  {
+    numero: 4275,
+    ano: 2018,
+    tema: "Direito de transgêneros à alteração de prenome e gênero no registro civil sem cirurgia",
+    relator: "Min. Marco Aurélio",
+    dataJulgamento: "01/03/2018",
+    resultado: "Procedente",
+    dispositivo: "Reconhecimento do direito independentemente de cirurgia de transgenitalização ou tratamentos hormonais.",
+    linkSTF: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=3723382"
+  },
+  {
+    numero: 6341,
+    ano: 2020,
+    tema: "Competência concorrente de Estados e Municípios em medidas de combate à pandemia",
+    relator: "Min. Marco Aurélio",
+    dataJulgamento: "15/04/2020",
+    resultado: "Parcialmente Procedente",
+    dispositivo: "Competência comum e concorrente de União, Estados, DF e Municípios na saúde pública.",
+    linkSTF: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=5880765"
+  }
+  // Insira as demais ADIs conforme o mesmo padrão
+];
