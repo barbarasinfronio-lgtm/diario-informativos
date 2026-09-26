d99cfa5e9fa536da9a50ad908cb9ebf44821f4c4
barbarasinfronio-lgtm <barbara.sinfronio@gmail.com>
2026-09-26 06:58:56 -0300

    Update adi_dados.js

--- controleconst/adi_dados.js
+++ controleconst/adi_dados.js
@@ -1,34 +1,40 @@
-// adi_dados.js - Base de Dados de ADIs julgadas pelo STF
+// controleconst/adi_dados.js
 const DADOS_ADIS = [
   {
-    numero: 4439,
-    ano: 2017,
-    tema: "Ensino religioso confessional nas escolas públicas",
-    relator: "Min. Roberto Barroso",
-    dataJulgamento: "27/09/2017",
-    resultado: "Improcedente", // Procedente, Improcedente, Parcialmente Procedente
-    dispositivo: "Faculdade de matrícula; natureza confessional do ensino religioso nas escolas públicas mantida constitucional.",
-    linkSTF: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=3921764"
-  },
-  {
-    numero: 4275,
-    ano: 2018,
-    tema: "Direito de transgêneros à alteração de prenome e gênero no registro civil sem cirurgia",
-    relator: "Min. Marco Aurélio",
-    dataJulgamento: "01/03/2018",
-    resultado: "Procedente",
-    dispositivo: "Reconhecimento do direito independentemente de cirurgia de transgenitalização ou tratamentos hormonais.",
-    linkSTF: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=3723382"
+    numero: 7300,
+    ano: 2026,
+    data: "20 fev 2026",
+    tema: "Reforma tributária sobre bens e serviços e repartição constitucional de receitas",
+    relator: "Min. Luís Roberto Barroso",
+    resultado: "Improcedente", // ou "Procedente", "Parcialmente Procedente"
+    url: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=6543210"
   },
   {
     numero: 6341,
     ano: 2020,
-    tema: "Competência concorrente de Estados e Municípios em medidas de combate à pandemia",
+    data: "15 abr 2020",
+    tema: "Competência concorrente de Estados e Municípios para medidas sanitárias e restritivas",
     relator: "Min. Marco Aurélio",
-    dataJulgamento: "15/04/2020",
     resultado: "Parcialmente Procedente",
-    dispositivo: "Competência comum e concorrente de União, Estados, DF e Municípios na saúde pública.",
-    linkSTF: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=5880765"
+    url: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=5880765"
+  },
+  {
+    numero: 4275,
+    ano: 2018,
+    data: "01 mar 2018",
+    tema: "Alteração de prenome e gênero de pessoa transgênero no registro civil sem necessidade de cirurgia",
+    relator: "Min. Edson Fachin",
+    resultado: "Procedente",
+    url: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=3723382"
+  },
+  {
+    numero: 4439,
+    ano: 2017,
+    data: "27 set 2017",
+    tema: "Constitucionalidade do ensino religioso confessional nas escolas públicas da rede oficial",
+    relator: "Min. Roberto Barroso",
+    resultado: "Improcedente",
+    url: "https://portal.stf.jus.br/processos/detalhe.asp?incidente=3921764"
   }
-  // Insira as demais ADIs conforme o mesmo padrão
+  // Adicione as demais ADIs seguindo a mesma estrutura
 ];
